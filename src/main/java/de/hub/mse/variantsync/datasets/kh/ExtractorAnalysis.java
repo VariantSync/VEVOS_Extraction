9c17ead15f7f820d7c432ac24f3e32746a6abfe3
Alexander Schultheiß <alexander.schultheiss@informatik.hu-berlin.de>
2021-03-29 17:14:23 +0200

    Updated TODOs and Docs

--- src/main/java/de/hub/mse/variantsync/datasets/kh/ExtractorAnalysis.java
+++ src/main/java/de/hub/mse/variantsync/datasets/kh/ExtractorAnalysis.java
@@ -1,19 +1,4 @@
 package de.hub.mse.variantsync.datasets.kh;
-/*
- * Copyright 2017-2019 University of Hildesheim, Software Systems Engineering
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 import net.ssehub.kernel_haven.SetUpException;
 import net.ssehub.kernel_haven.analysis.AbstractAnalysis;
@@ -42,7 +27,6 @@ import java.io.IOException;
  *
  * @author Alex S.
  */
-// TODO: Refactor to AnalysisComponent
 public class ExtractorAnalysis extends AbstractAnalysis {
     public static final @NonNull Setting<@Nullable String> WORK_DIR
             = new Setting<>("working_directory", Setting.Type.STRING, false, null, "" +
