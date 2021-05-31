0835f5475914ce996dcb7300f4e14650b10e9b7e
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-05-31 21:16:29 +0000

    Trying out different ways to adjust Makefiles

--- src/main/java/de/variantsync/subjects/extraction/AnalysisTask.java
+++ src/main/java/de/variantsync/subjects/extraction/AnalysisTask.java
@@ -243,15 +243,16 @@ public class AnalysisTask implements Runnable {
 
         // Remove all error flags
         List<String> fixedLines = new ArrayList<>(Objects.requireNonNull(lines).size());
+        // TODO: Figure out how to adjust Makefiles correclty
         for (String line : lines) {
             // Replace "-Wall" with "-Wno-error"
             line = line.replaceAll("-Wall", "-Wno-error");
             // Replace "-Werror=SOMETHING" with "-Wno-error=SOMETHING"
-            line = line.replaceAll("-Werror=", "-Wno-error=");
+//            line = line.replaceAll("-Werror=", "-Wno-error=");
             // Replace all remaining error flags, that follow the pattern "-WSOMETHING", with "-Wno-error=SOMETHING"
-            line = line.replaceAll("(?!-Wno-error)-W", "-Wno-error=");
+//            line = line.replaceAll("(?!-Wno-error)-W", "-Wno-error=");
             // Replace all cases with the construct "-Wno-error=SOMETHING=VALUE" with "-Wno-error"
-            line = line.replaceAll("-Wno-error=[\\S]+=[\\S]+", "-Wno-error=");
+//            line = line.replaceAll("-Wno-error=[\\S]+=[\\S]+", "-Wno-error=");
             fixedLines.add(line);
         }
 
