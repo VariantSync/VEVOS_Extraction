8b58f7bd5147e026fbb2f0e05b4e32f402c0c0bf
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-06-03 12:16:18 +0000

    Added adjusted versions of KernelHaven modules and deploy script

---
+++ deploy-kernelhaven.sh
@@ -0,0 +1,25 @@
+# mvn deploy:deploy-file -DgroupId=net.ssehub -DartifactId=kernel_haven -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/KernelHaven.jar
+
+mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=busyboot -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/BusybootPreparation_jar/BusybootPreparation.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=cnf -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/CnfUtils.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=block_extractor -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/CodeBlockExtractor.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=cpp_utils -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/CppUtils.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=db -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/DBUtils.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=fe_analysis -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/FeatureEffectAnalysis.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=io -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/IOUtils.jar
+
+mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=kbuildminer -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/KbuildMinerExtractor_jar/KbuildMinerExtractor.jar
+
+mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=kconfigreader -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/KconfigReaderExtractor_jar/KconfigReaderExtractor.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=non_boolean -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/NonBooleanUtils.jar
+
+# mvn deploy:deploy-file -DgroupId=net.ssehub.kernel_haven -DartifactId=typechef -Dversion=1.0.0 -Durl=file:./local-maven-repo/ -DrepositoryId=local-maven-repo -DupdateReleaseInfo=true -Dfile=/home/alex/dev/KernelHavenModules/bin/artifacts/TypeChefExtractor.jar
+
+rm -rf ~/.m2/repository/net
