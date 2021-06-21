0af54aa18661ad6734a0b384e30bee940abb1c9a
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-06-21 12:37:06 +0000

    Added removal of old results to run scripts

--- src/main/resources/busybox-run.sh
+++ src/main/resources/busybox-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Removing previous results"
+rm -rf extraction-results
 echo "Executing variability extraction of BusyBox."
 java -jar VariabilityExtraction-1.0.0-jar-with-dependencies.jar variability_analysis_BusyBox.properties
--- src/main/resources/linux-run.sh
+++ src/main/resources/linux-run.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
+echo "Removing previous results"
+rm -rf extraction-results
 echo "Executing variability extraction of Linux."
 java -jar VariabilityExtraction-1.0.0-jar-with-dependencies.jar variability_analysis_Linux.properties v4.5 v4.6
