88de6e247da13d96b8ac09c7514e9a14e077f1a7
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-06-21 19:56:26 +0000

    modified build files

--- extract-busybox.sh
+++ extract-busybox.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+echo "Removing results directory for BusyBox"
+rm -rf extraction-results/busybox
 docker run -v "$(pwd)/extraction-results/busybox:/variability-extraction/extraction-results" variability-extraction busybox
--- extract-linux.sh
+++ extract-linux.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
+echo "Removing results directory for linux"
+rm -rf extraction-results/linux
 docker run -v "$(pwd)/extraction-results/linux:/variability-extraction/extraction-results" variability-extraction linux
