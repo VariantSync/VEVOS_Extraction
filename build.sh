75f80ae234fa923ef64428a3d3062aba67df6b73
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-06-21 20:10:02 +0000

    Some script adjustments

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
+rm -rf extraction-results
 docker build -t variability-extraction .
--- src/main/resources/start-extraction.sh
+++ src/main/resources/start-extraction.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 echo "Starting extraction"
+gcc --version
+java -version
 if [ "$1" = 'busybox' ]
 then
     echo $(git clone https://git.busybox.net/busybox/)
     ./busybox-run.sh
 elif [ "$1" = 'linux' ]
 then
-echo "Cloning Linux, this will take quite some time."
+    echo "Cloning Linux, this will take quite some time."
     echo $(git clone https://github.com/torvalds/linux.git)
     ./linux-run.sh
 else
