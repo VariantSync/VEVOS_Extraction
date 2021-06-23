6ebedf793e2b87d1ab8236bfee1815b2284bf5ae
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-06-23 11:11:16 +0000

    Working on potential fix

--- start-linux-extraction.sh
+++ start-linux-extraction.sh
@@ -10,7 +10,7 @@ docker container stop variability-extraction-linux
 
 echo "Copying data from the Docker container to ./extraction-results/linux"
 mkdir -p extraction-results/linux
-docker run --rm --volumes-from variability-extraction-linux --user "$(id -u)" -v "$(pwd)/extraction-results/linux":/home/$(id -u)/data ubuntu cp -rf /home/user/extraction-results ~/data || exit
+docker run --rm --volumes-from variability-extraction-linux --user "$(id -u)" --workdir="/home/$(id -u)" -v "$(pwd)/extraction-results/linux":data ubuntu cp -rf /home/user/extraction-results ~/data || exit
 
 echo "Removing Docker container and volume"
 docker container rm variability-extraction-linux
--- stop-busybox-extraction.sh
+++ stop-busybox-extraction.sh
@@ -4,7 +4,9 @@ docker container stop variability-extraction-busybox
 
 echo "Copying data from the Docker container to ./extraction-results/busybox"
 mkdir -p extraction-results/busybox
-docker run --rm --volumes-from variability-extraction-busybox --user "$(id -u)" -v "$(pwd)/extraction-results/busybox":/home/$(id -u)/data ubuntu cp -rf /home/user/extraction-results ~/data || exit
+docker run --rm --volumes-from variability-extraction-busybox \
+-v "$(pwd)/extraction-results/busybox":"/home/user/data" \
+ubuntu cp -rf /home/user/extraction-results /home/user/data || exit
 
 echo "Removing Docker container and volume"
 docker container rm variability-extraction-busybox
