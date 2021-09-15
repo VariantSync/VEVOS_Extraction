c5f94e3671d8ac4f313846295adbb5039350d356
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-09-15 10:22:16 +0200

    Changed start and stop script for Windows

--- start-extraction.bat
+++ start-extraction.bat
@@ -7,55 +7,20 @@
 @GOTO AFTER
 
 :BUSYBOX
-@echo Deleting old busybox data under ./extraction-results/busybox
-@del /s /f /q extraction-results\busybox\*.*
-@for /f %%f in ('dir /ad /b extraction-results\busybox\') do rd /s /q extraction-results\busybox\%%f
+@if not exist "extraction-results" mkdir extraction-results
+@if not exist "extraction-results/busybox" mkdir extraction-results/busybox
 
 @echo "Starting the extraction"
-@docker run --user "1000:1000" --name variability-extraction-busybox --mount source=busybox-extraction,target=/home/user/extraction-results/output variability-extraction %*
+@docker run --rm -v "%cd%/extraction-results/busybox":"/home/user/extraction-results/output" variability-extraction %*
 
-@echo "Copying data from the Docker container to ./extraction-results/busybox"
-@md extraction-results
-@cd extraction-results
-@md busybox
-@cd ..
-
-@docker run --rm --volumes-from variability-extraction-busybox^
-    -u "1000:1000"^
-    -v "%cd%/extraction-results/busybox":"/home/user/data"^
-    ubuntu cp -rf /home/user/extraction-results/output /home/user/data
-
-@echo "Removing Docker container and volume"
-@docker container rm variability-extraction-busybox
-@docker volume rm busybox-extraction
 @GOTO AFTER
 
 :LINUX
-@echo Delete the old linux data
-@del /s /f /q extraction-results\linux\*.*
-@for /f %%f in ('dir /ad /b extraction-results\linux\') do rd /s /q extraction-results\linux\%%f
+@if not exist "extraction-results" mkdir extraction-results
+@if not exist "extraction-results/linux" mkdir extraction-results/linux
 
 @echo "Starting the extraction"
-@docker run^
-    --user "1000:1000"^
-    --name variability-extraction-linux^
-    --mount source=linux-extraction,target=/home/user/extraction-results/output^
-    variability-extraction %*
-
-@echo "Copying data from the Docker container to ./extraction-results/linux"
-@md extraction-results
-@cd extraction-results
-@md linux
-@cd ..
-
-@docker run --rm --volumes-from variability-extraction-linux^
-    -u "1000:1000"^
-    -v "%cd%/extraction-results/linux":"/home/user/data"^
-    ubuntu cp -rf /home/user/extraction-results/output /home/user/data
-
-@echo "Removing Docker container and volume"
-@docker container rm variability-extraction-linux
-@docker volume rm linux-extraction
+@docker run --rm -v "%cd%/extraction-results/linux":"/home/user/extraction-results/output" variability-extraction %*
 
 :AFTER
 @pause
--- stop-extraction.bat
+++ stop-extraction.bat
@@ -1,19 +1,4 @@
-@echo "Stopping extraction"
-
-@if "%1"=="busybox" GOTO BUSYBOX
-@if "%1"=="linux" GOTO LINUX
-
-@echo "Select a process to stop [ ./stop-extraction.bat linux | ./stop-extraction.bat busybox ]"
-@GOTO AFTER
-
-:BUSYBOX
-@echo "Stopping busybox extraction"
-@docker container stop variability-extraction-busybox
-@GOTO AFTER
-
-:LINUX
-@echo "Stopping linux extraction"
-@docker container stop variability-extraction-linux
-
-:AFTER
+@echo "Stopping all running extractions. This will take a moment..."
+@FOR /f "tokens=*" %%i IN ('docker ps -a -q --filter "ancestor=variability-extraction"') DO docker stop %%i
+@echo "...done."
 pause
