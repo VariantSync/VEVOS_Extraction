02f2e50f75f4c665c5a41e5abc7670e8135abc1f
Alexander Schultheiß <AlexanderSchultheiss@pm.me>
2021-09-23 17:27:51 +0200

    Minor setup changes

--- start-extraction.sh
+++ start-extraction.sh
@@ -1,11 +1,9 @@
 #! /bin/bash
 if [ "$1" = 'busybox' ]
 then
-    mkdir -p extraction-results/busybox
     docker run --rm -v "$(pwd)/extraction-results/busybox":"/home/user/extraction-results/output" variability-extraction "$@"
 elif [ "$1" = 'linux' ]
 then
-    mkdir -p extraction-results/linux
     docker run --rm -v "$(pwd)/extraction-results/linux":"/home/user/extraction-results/output" variability-extraction "$@"
 else
     echo "Select a SPL to extract [ ./start-extraction.sh linux | ./start-extraction.sh busybox ]"
